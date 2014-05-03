42718927c32e96d31056f783757fe2c70a5da5bb
Ralf Cheung <ralfcheung@guest-wireless-nup-nat-206-117-89-009.usc.edu>
2014-05-03 00:58:14 -0700

    initial commit

---
+++ ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Fashion
+//
+//  Created by Ralf Cheung on 2/13/14.
+//  Copyright (c) 2014 Ralf Cheung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Accounts/Accounts.h>
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
+
+@end
